08924fe5eb3e87591dc56e392c3d296517671251
CarlMarcus <gongzhizhi@outlook.com>
2019-06-29 23:33:00 +0800

    2019/06/29

---
+++ AgentDemo/src/main/java/com/github/PreAgent.java
@@ -0,0 +1,17 @@
+package com.github;
+
+
+import java.lang.instrument.Instrumentation;
+
+public class PreAgent
+{
+    public static void premain(String agentOps, Instrumentation inst) {
+        System.out.println("=========premain方法执行1========");
+        System.out.println(agentOps);
+    }
+
+    public static void premain(String agentOps) {
+        System.out.println("=========premain方法执行2========");
+        System.out.println(agentOps);
+    }
+}
---
+++ AgentDemo/src/test/java/com/github/PreAgentTest.java
@@ -0,0 +1,20 @@
+package com.github;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple PreAgent.
+ */
+public class PreAgentTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
