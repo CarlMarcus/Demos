e276c2ce706bc51bdc9092d80e1ec587ce8708ea
CarlMarcus <gongzhizhi@outlook.com>
2019-07-07 20:37:42 +0800

    2019/07/07

--- Concurrency/src/com/github/JoinWaitSleepInterrupt/InterruptDemo.java
+++ Concurrency/src/com/github/JoinWaitSleepInterrupt/InterruptDemo.java
@@ -2,21 +2,35 @@ package com.github.JoinWaitSleepInterrupt;
 
 public class InterruptDemo implements Runnable {
 
-    Thread t = new Thread(this);
+    Thread t;
+    public InterruptDemo() {
+        t = new Thread(this);
+        System.out.println("Executing " + t.getName());
+        t.start();
+
+        if (!t.isInterrupted()) {
+            t.interrupt();
+        } // interrupt the threads
+
+        try {
+            t.join();
+        } catch (InterruptedException e) { } // block until other threads finish
+    }
 
     @Override
     public void run() {
         try {
             while (true)
-                Thread.sleep(1000);
+                Thread.sleep(1);
         } catch (InterruptedException e) {
-            System.out.print(t.getName() + " interrupted:");
-            e.printStackTrace();
+            System.out.print(t.getName() + " interrupted: ");
+            System.out.println(e.toString());
         }
     }
 
     public static void main(String[] args) {
-
+        new InterruptDemo();
+        new InterruptDemo();
     }
 
 }
