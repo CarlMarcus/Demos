1d924b332dc044e4eb9e204d7183c805c396d668
CarlMarcus <gongzhizhi@outlook.com>
2019-07-29 15:16:52 +0800

    ...

---
+++ Concurrency/src/com/github/ThreadLocalDemo/ThreadLocalDemo.java
@@ -0,0 +1,42 @@
+package com.github.ThreadLocalDemo;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// SimpleDateFormat.parse方法会有线程安全的问题，
+// 可以尝试使用threadLocal包装SimpleDateFormat，将该实例不被多线程共享即可。
+public class ThreadLocalDemo {
+    private static ThreadLocal<SimpleDateFormat> sdf = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(new DateUtil("2019-11-25 09:00:" + i % 60));
+        }
+    }
+
+    private static class DateUtil implements Runnable {
+        private String date;
+
+        public DateUtil(String date) {
+            this.date = date;
+        }
+
+        @Override
+        public void run() {
+            if (sdf.get() == null) {
+                sdf.set(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+            } else {
+                try {
+                    Date date = sdf.get().parse(this.date);
+                    System.out.println(date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
